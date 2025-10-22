319250d1bf3e3a68e9d5a9ebc88cabd4061ddd96
Wilode <114880028+WaniaJP@users.noreply.github.com>
2025-10-22 15:18:21 +0200

    Update Input Handler to work Offline

--- Scripts/InputHandler.gd
+++ Scripts/InputHandler.gd
@@ -17,53 +17,77 @@ func _ready() -> void:
 		print("ESP connectée")
 		is_connected = true
 	else:
-		push_error("Impossible de se connecter")
+		printerr("Impossible de se connecter")
 		is_connected = false
 
 func _process(delta: float) -> void:
-	if not is_connected:
-		return
-
-	monitor_serial()
+	if(waiting_for_button):
+		monitor_serial()
 
 func request_button_input() -> void:
-	if not is_connected:
-		push_warning("Pas de connexion")
-		return
-
 	if waiting_for_button:
-		push_warning("Déjà en attente d’un bouton")
+		print("WARNING => Déjà en attente d’un bouton")
 		return
-
-	var request = {"getButton": true}
-	var msg = JSON.stringify(request)
-	serial.write_string(msg + "\n")
+		
+	if not is_connected:
+		print("WARNING => Pas de connexion")
+	else:
+		var request = {"getButton": true}
+		var msg = JSON.stringify(request)
+		serial.write_string(msg + "\n")
 	waiting_for_button = true
 	print("Demande d’input bouton envoyée")
 	
 func monitor_serial() -> void:
-	var bytes = serial.bytes_available()
-	if bytes <= 0:
-		return
+	if is_connected:
+		var bytes = serial.bytes_available()
+		if bytes <= 0:
+			return
 
-	var data = serial.readline()
-	data = data.strip_edges()
+		var data = serial.readline()
+		data = data.strip_edges()
+		
+		if data == "":
+			return
 
-	if data == "":
-		return
+		var json := JSON.new()
+		if json.parse(data) != OK:
+			print("WARNING => JSON invalide reçu : " + data)
+			return
 
-	var json := JSON.new()
-	if json.parse(data) != OK:
-		push_warning("JSON invalide reçu : " + data)
-		return
+		var obj = json.get_data()
+		if obj.has("type") and obj["type"] == "button_state":
+			current_button = obj["pressed"]
+			print("Bouton détecté : ", current_button)
+			emit_signal("button_pressed", current_button)
+			send_ack()
+			waiting_for_button = false
+	else:
+		if Input.is_action_just_pressed("buttonOne"):
+			current_button = "button_1"
+			print("Bouton détecté : ", current_button)
+			emit_signal("button_pressed", current_button)
+			waiting_for_button = false
+			return
+		if Input.is_action_just_pressed("buttonTwo"):
+			current_button = "button_2"
+			print("Bouton détecté : ", current_button)
+			emit_signal("button_pressed", current_button)
+			waiting_for_button = false
+			return
+		if Input.is_action_just_pressed("buttonThree"):
+			current_button = "button_3"
+			print("Bouton détecté : ", current_button)
+			emit_signal("button_pressed", current_button)
+			waiting_for_button = false
+			return	
+		if Input.is_action_just_pressed("buttonFour"):
+			current_button = "button_4"
+			print("Bouton détecté : ", current_button)
+			emit_signal("button_pressed", current_button)
+			waiting_for_button = false
+			return
 
-	var obj = json.get_data()
-	if obj.has("type") and obj["type"] == "button_state":
-		current_button = obj["pressed"]
-		print("Bouton détecté : ", current_button)
-		emit_signal("button_pressed", current_button)
-		send_ack()
-		waiting_for_button = false
 		
 func send_ack() -> void:
 	var ack = {"ack": true}
--- Scripts/TestInput.gd
+++ Scripts/TestInput.gd
@@ -4,7 +4,6 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("hello")
 	inputHandler.button_pressed.connect(_on_button_pressed)
 
 
